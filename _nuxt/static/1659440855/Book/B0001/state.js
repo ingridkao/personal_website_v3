ddb8e28793bebec43a2920f7f41c3977c03ee50b
ingridkao <ingridkao@users.noreply.github.com>
2022-08-02 11:50:16 +0000

    deploy: c1fc890bbf3ce882d5c91c90a65bc40c7bd4f96b

--- _nuxt/static/1659440855/Book/B0001/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659440855",layout:"default",error:a,serverRendered:true,routePath:"\u002FBook\u002FB0001",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"290f971c"}}}}(null));
