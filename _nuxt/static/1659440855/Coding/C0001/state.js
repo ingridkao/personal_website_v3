a5df56555f4303768ad87be855656699a883b36b
ingridkao <ingridkao@users.noreply.github.com>
2022-08-02 11:47:37 +0000

    deploy: 4d4117a34efd801af3d78280d05aa15965235d93

---
+++ _nuxt/static/1659440855/Coding/C0001/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659440855",layout:"default",error:a,serverRendered:true,routePath:"\u002FCoding\u002FC0001",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"290f971c"}}}}(null));
---
+++ _nuxt/static/1659440855/Investment/test/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/Investment/test", {data:[{}],fetch:{},mutations:void 0});
