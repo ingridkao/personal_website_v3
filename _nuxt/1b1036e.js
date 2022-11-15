b0cde9a1c6d8b620104d98e126e4ea821680f635
ingridkao <ingridkao@users.noreply.github.com>
2022-11-15 11:03:46 +0000

    deploy: d8aa3e7c23b9afc4eb819c14a18b64fe2bf2fbb6

--- _nuxt/1b1036e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{527:function(n,e,t){"use strict";t.r(e);var l={name:"AboutPage"},o=t(61),component=Object(o.a)(l,(function(){var n=this._self._c;return n("main",[n("BlogMD")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
